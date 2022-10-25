b846cc9c9e6b454b720ea12342810da5a83c839f
Pankaj-Patil22 <pkjpatil1011@gmail.com>
2022-10-25 15:29:44 +0530

    renamed the app

--- Controllers/application.py
+++ Controllers/application.py
@@ -5,13 +5,14 @@ import traceback
  
 class FlaskAppWrapper():
     def __init__(self):
+        __name__ = '__main__'
         self.application = Flask(__name__)
+        print("has to be ",__name__)
         self._config()
-
-        @self.application.route('/', methods=['GET'])
-        def index():
-            return jsonify({"success": True}), 200
         
+        @self.application.route('/', methods = ['GET'])
+        def index():
+            return jsonify({'message': 'Hello World!'})
     
     def get_app(self):
         return self.application
@@ -19,20 +20,16 @@ class FlaskAppWrapper():
     def _config(self):
         CORS(self.application)
         self.application.secret_key = os.urandom(24)
+    
+    
 
-
-    # def run(self, debug,  host, port):
-    #     self.application.run(debug=debug, host=host, port=port)
-
-    def run(self):
-        self.application.debug = True
-        self.application.run()
+    def run(self, debug,  host, port):
+        self.application.run(debug=debug, host=host, port=port)
 
 
 def main():
     application = FlaskAppWrapper()
-    # application.run(True, host='0.0.0.0', port=5000)
-    application.run()
+    application.run(True, host='0.0.0.0', port=5000)
     return application
 
 if __name__ == '__main__':
